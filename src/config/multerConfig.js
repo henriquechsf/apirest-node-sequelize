9050d7e6820497638bc609e35fd57fa2f82d4100
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2020-12-16 23:17:54 -0300

    tratando os arquivos enviados

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,13 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  // validação para tipo de arquivo
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo deve ser PNG ou JPG'));
+    }
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     // onde sera salvo as fotos
     destination: (req, file, cb) => {
--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+// captura o arquivo enviado
+const upload = multer(multerConfig).single('arquivo');
+
 class FotoController {
   async create(req, res) {
-    res.json('Foto');
+    return upload(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return res.json(req.file);
+    });
   }
 }
 
